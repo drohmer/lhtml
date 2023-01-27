d9f9dc0531f0aab07895e09d815e81d2ac08cfce
Damien Rohmer <damien.rohmer@gmail.com>
2023-01-27 22:11:54 +0100

    Code language

--- src/lhtmlLib/code.py
+++ src/lhtmlLib/code.py
@@ -5,7 +5,7 @@ from pygments.formatters import HtmlFormatter
 
 def export_html_code(text, language, is_linenos=False, cssclass='code'):
 
-    lexer = get_lexer_by_name("c++", stripall=False, stripnl=True, ensurenl='True', tabsize=2, encoding='utf-8')
+    lexer = get_lexer_by_name(language, stripall=False, stripnl=True, ensurenl='True', tabsize=2, encoding='utf-8')
     formatter = HtmlFormatter(is_linenos=False, cssclass="code")
     html = highlight(text, lexer, formatter)
 
